93a63e9e2fd923a65c086ad82ff8c4760902c162
YamiCisneros <ymcisneros25@gmail.com>
2024-05-04 17:52:14 -0300

    prueba

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace GrupoE_Protitipos
+﻿
+namespace GrupoE_Protitipos
 {
     partial class Menu
     {
@@ -34,13 +35,15 @@
             button4 = new Button();
             button5 = new Button();
             button6 = new Button();
+            button7 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(165, 43);
+            button1.Location = new Point(144, 32);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(308, 47);
+            button1.Size = new Size(270, 35);
             button1.TabIndex = 0;
             button1.Text = "Informar espacio disponible";
             button1.UseVisualStyleBackColor = true;
@@ -48,64 +51,85 @@
             // 
             // button2
             // 
-            button2.Location = new Point(165, 176);
+            button2.Location = new Point(144, 132);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(308, 47);
+            button2.Size = new Size(270, 35);
             button2.TabIndex = 1;
             button2.Text = "Procesamiento orden de preparacion";
             button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(165, 111);
+            button3.Location = new Point(144, 83);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(308, 47);
+            button3.Size = new Size(270, 35);
             button3.TabIndex = 2;
             button3.Text = "Orden recepción";
             button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new Point(165, 238);
+            button4.Location = new Point(144, 178);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(308, 47);
+            button4.Size = new Size(270, 35);
             button4.TabIndex = 3;
             button4.Text = "Orden de seleccion";
             button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            button5.Location = new Point(165, 301);
+            button5.Location = new Point(144, 226);
+            button5.Margin = new Padding(3, 2, 3, 2);
             button5.Name = "button5";
-            button5.Size = new Size(308, 47);
+            button5.Size = new Size(270, 35);
             button5.TabIndex = 4;
             button5.Text = "Orden de entrega";
             button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            button6.Location = new Point(165, 366);
+            button6.Location = new Point(144, 274);
+            button6.Margin = new Padding(3, 2, 3, 2);
             button6.Name = "button6";
-            button6.Size = new Size(308, 47);
+            button6.Size = new Size(270, 35);
             button6.TabIndex = 5;
             button6.Text = "Estado de ordenes";
             button6.UseVisualStyleBackColor = true;
             // 
+            // button7
+            // 
+            button7.Location = new Point(580, 153);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 6;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
             // Menu
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(button7);
             Controls.Add(button6);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Menu";
             Text = "Menu Principal";
-            Load += this.Menu_Load;
+            Load += Menu_Load;
             ResumeLayout(false);
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
@@ -116,5 +140,6 @@
         private Button button4;
         private Button button5;
         private Button button6;
+        private Button button7;
     }
 }
