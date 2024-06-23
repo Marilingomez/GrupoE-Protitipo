29f41affc96e554b49871b987cfe8d5b90a7323b
Alan <gimenezalan00@gmail.com>
2024-06-22 21:30:53 -0300

    muestra nombre de clientes p1, cambia label de producto

--- Formularios/AOrdenDePreparacion/FormOrdenDePreparacion.Designer.cs
+++ Formularios/AOrdenDePreparacion/FormOrdenDePreparacion.Designer.cs
@@ -63,9 +63,9 @@
             Orden.AutoSize = true;
             Orden.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
             Orden.ForeColor = Color.FromArgb(161, 32, 89);
-            Orden.Location = new Point(12, 52);
+            Orden.Location = new Point(10, 39);
             Orden.Name = "Orden";
-            Orden.Size = new Size(174, 20);
+            Orden.Size = new Size(133, 15);
             Orden.TabIndex = 0;
             Orden.Text = "Ordenes de preparacion";
             // 
@@ -73,11 +73,10 @@
             // 
             DetallesList.Columns.AddRange(new ColumnHeader[] { IDProducto, DescripcionProducto, Cantidad });
             DetallesList.FullRowSelect = true;
-            DetallesList.Location = new Point(12, 320);
-            DetallesList.Margin = new Padding(3, 4, 3, 4);
+            DetallesList.Location = new Point(10, 240);
             DetallesList.MultiSelect = false;
             DetallesList.Name = "DetallesList";
-            DetallesList.Size = new Size(724, 287);
+            DetallesList.Size = new Size(634, 216);
             DetallesList.TabIndex = 5;
             DetallesList.UseCompatibleStateImageBehavior = false;
             DetallesList.View = View.Details;
@@ -103,64 +102,62 @@
             DetalleOrden.AutoSize = true;
             DetalleOrden.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
             DetalleOrden.ForeColor = Color.FromArgb(161, 32, 89);
-            DetalleOrden.Location = new Point(12, 296);
+            DetalleOrden.Location = new Point(10, 222);
             DetalleOrden.Name = "DetalleOrden";
-            DetalleOrden.Size = new Size(151, 20);
+            DetalleOrden.Size = new Size(117, 15);
             DetalleOrden.TabIndex = 6;
             DetalleOrden.Text = "Detalle de productos";
             // 
             // IdProductoLabel
             // 
             IdProductoLabel.AutoSize = true;
-            IdProductoLabel.Location = new Point(12, 156);
+            IdProductoLabel.Location = new Point(10, 117);
             IdProductoLabel.Name = "IdProductoLabel";
-            IdProductoLabel.Size = new Size(86, 20);
+            IdProductoLabel.Size = new Size(56, 15);
             IdProductoLabel.TabIndex = 7;
-            IdProductoLabel.Text = "Id Producto";
+            IdProductoLabel.Text = "Producto";
             // 
             // Fecha
             // 
             Fecha.AutoSize = true;
-            Fecha.Location = new Point(542, 95);
+            Fecha.Location = new Point(474, 71);
             Fecha.Name = "Fecha";
-            Fecha.Size = new Size(47, 20);
+            Fecha.Size = new Size(38, 15);
             Fecha.TabIndex = 9;
             Fecha.Text = "Fecha";
             // 
             // FechaBox
             // 
             FechaBox.Enabled = false;
-            FechaBox.Location = new Point(595, 92);
-            FechaBox.Margin = new Padding(3, 4, 3, 4);
+            FechaBox.Location = new Point(521, 69);
             FechaBox.MaxLength = 8;
             FechaBox.Name = "FechaBox";
-            FechaBox.Size = new Size(129, 27);
+            FechaBox.Size = new Size(113, 23);
             FechaBox.TabIndex = 10;
             // 
             // IdClienteLabel
             // 
             IdClienteLabel.AutoSize = true;
-            IdClienteLabel.Location = new Point(274, 99);
+            IdClienteLabel.Location = new Point(240, 74);
             IdClienteLabel.Name = "IdClienteLabel";
-            IdClienteLabel.Size = new Size(55, 20);
+            IdClienteLabel.Size = new Size(44, 15);
             IdClienteLabel.TabIndex = 11;
             IdClienteLabel.Text = "Cliente";
             // 
             // CantidadBox
             // 
-            CantidadBox.Location = new Point(375, 156);
-            CantidadBox.Margin = new Padding(3, 4, 3, 4);
+            CantidadBox.Location = new Point(328, 117);
             CantidadBox.MaxLength = 3;
             CantidadBox.Name = "CantidadBox";
-            CantidadBox.Size = new Size(138, 27);
+            CantidadBox.Size = new Size(121, 23);
             CantidadBox.TabIndex = 14;
             // 
             // CantidadLabel
             // 
             CantidadLabel.AutoSize = true;
-            CantidadLabel.Location = new Point(274, 163);
+            CantidadLabel.Location = new Point(240, 122);
             CantidadLabel.Name = "CantidadLabel";
-            CantidadLabel.Size = new Size(69, 20);
+            CantidadLabel.Size = new Size(55, 15);
             CantidadLabel.TabIndex = 13;
             CantidadLabel.Text = "Cantidad";
             CantidadLabel.Click += CantidadLabel_Click;
@@ -169,10 +166,9 @@
             // 
             AgregarBoton.BackColor = Color.FromArgb(161, 32, 89);
             AgregarBoton.ForeColor = SystemColors.ButtonHighlight;
-            AgregarBoton.Location = new Point(589, 280);
-            AgregarBoton.Margin = new Padding(3, 4, 3, 4);
+            AgregarBoton.Location = new Point(515, 210);
             AgregarBoton.Name = "AgregarBoton";
-            AgregarBoton.Size = new Size(147, 32);
+            AgregarBoton.Size = new Size(129, 24);
             AgregarBoton.TabIndex = 21;
             AgregarBoton.Text = "Agregar";
             AgregarBoton.UseVisualStyleBackColor = false;
@@ -182,10 +178,9 @@
             // 
             EliminarBoton.BackColor = Color.FromArgb(161, 32, 89);
             EliminarBoton.ForeColor = SystemColors.ButtonHighlight;
-            EliminarBoton.Location = new Point(429, 615);
-            EliminarBoton.Margin = new Padding(3, 4, 3, 4);
+            EliminarBoton.Location = new Point(375, 461);
             EliminarBoton.Name = "EliminarBoton";
-            EliminarBoton.Size = new Size(147, 32);
+            EliminarBoton.Size = new Size(129, 24);
             EliminarBoton.TabIndex = 22;
             EliminarBoton.Text = "Eliminar";
             EliminarBoton.UseVisualStyleBackColor = false;
@@ -195,10 +190,9 @@
             // 
             ConfirmarBoton.BackColor = Color.FromArgb(161, 32, 89);
             ConfirmarBoton.ForeColor = SystemColors.ButtonHighlight;
-            ConfirmarBoton.Location = new Point(600, 615);
-            ConfirmarBoton.Margin = new Padding(3, 4, 3, 4);
+            ConfirmarBoton.Location = new Point(525, 461);
             ConfirmarBoton.Name = "ConfirmarBoton";
-            ConfirmarBoton.Size = new Size(136, 32);
+            ConfirmarBoton.Size = new Size(119, 24);
             ConfirmarBoton.TabIndex = 23;
             ConfirmarBoton.Text = "Confirmar";
             ConfirmarBoton.UseVisualStyleBackColor = false;
@@ -208,10 +202,9 @@
             // 
             SalirBoton.BackColor = Color.FromArgb(161, 32, 89);
             SalirBoton.ForeColor = SystemColors.ButtonHighlight;
-            SalirBoton.Location = new Point(600, 655);
-            SalirBoton.Margin = new Padding(3, 4, 3, 4);
+            SalirBoton.Location = new Point(525, 491);
             SalirBoton.Name = "SalirBoton";
-            SalirBoton.Size = new Size(136, 32);
+            SalirBoton.Size = new Size(119, 24);
             SalirBoton.TabIndex = 24;
             SalirBoton.Text = "Volver al menu";
             SalirBoton.UseVisualStyleBackColor = false;
@@ -220,28 +213,27 @@
             // DepositoLabel
             // 
             DepositoLabel.AutoSize = true;
-            DepositoLabel.Location = new Point(12, 215);
+            DepositoLabel.Location = new Point(10, 161);
             DepositoLabel.Name = "DepositoLabel";
-            DepositoLabel.Size = new Size(70, 20);
+            DepositoLabel.Size = new Size(54, 15);
             DepositoLabel.TabIndex = 15;
             DepositoLabel.Text = "Deposito";
             // 
             // IdOrdenBox
             // 
             IdOrdenBox.Enabled = false;
-            IdOrdenBox.Location = new Point(108, 92);
-            IdOrdenBox.Margin = new Padding(3, 4, 3, 4);
+            IdOrdenBox.Location = new Point(94, 69);
             IdOrdenBox.MaxLength = 10;
             IdOrdenBox.Name = "IdOrdenBox";
-            IdOrdenBox.Size = new Size(153, 27);
+            IdOrdenBox.Size = new Size(134, 23);
             IdOrdenBox.TabIndex = 26;
             // 
             // IdOrdenLabel
             // 
             IdOrdenLabel.AutoSize = true;
-            IdOrdenLabel.Location = new Point(12, 103);
+            IdOrdenLabel.Location = new Point(10, 77);
             IdOrdenLabel.Name = "IdOrdenLabel";
-            IdOrdenLabel.Size = new Size(67, 20);
+            IdOrdenLabel.Size = new Size(53, 15);
             IdOrdenLabel.TabIndex = 25;
             IdOrdenLabel.Text = "Id Orden";
             // 
@@ -249,38 +241,35 @@
             // 
             DepositoBox.DropDownStyle = ComboBoxStyle.DropDownList;
             DepositoBox.FormattingEnabled = true;
-            DepositoBox.Location = new Point(108, 211);
-            DepositoBox.Margin = new Padding(3, 4, 3, 4);
+            DepositoBox.Location = new Point(94, 158);
             DepositoBox.Name = "DepositoBox";
-            DepositoBox.Size = new Size(153, 28);
+            DepositoBox.Size = new Size(134, 23);
             DepositoBox.TabIndex = 27;
             // 
             // ClienteBox
             // 
             ClienteBox.DropDownStyle = ComboBoxStyle.DropDownList;
             ClienteBox.FormattingEnabled = true;
-            ClienteBox.Location = new Point(375, 95);
-            ClienteBox.Margin = new Padding(3, 4, 3, 4);
+            ClienteBox.Location = new Point(328, 71);
             ClienteBox.Name = "ClienteBox";
-            ClienteBox.Size = new Size(138, 28);
+            ClienteBox.Size = new Size(121, 23);
             ClienteBox.TabIndex = 28;
             // 
             // productoBox
             // 
             productoBox.DropDownStyle = ComboBoxStyle.DropDownList;
             productoBox.FormattingEnabled = true;
-            productoBox.Location = new Point(108, 153);
-            productoBox.Margin = new Padding(3, 4, 3, 4);
+            productoBox.Location = new Point(94, 115);
             productoBox.Name = "productoBox";
-            productoBox.Size = new Size(153, 28);
+            productoBox.Size = new Size(134, 23);
             productoBox.TabIndex = 29;
             // 
             // transportistaLabel
             // 
             transportistaLabel.AutoSize = true;
-            transportistaLabel.Location = new Point(274, 218);
+            transportistaLabel.Location = new Point(240, 164);
             transportistaLabel.Name = "transportistaLabel";
-            transportistaLabel.Size = new Size(94, 20);
+            transportistaLabel.Size = new Size(74, 15);
             transportistaLabel.TabIndex = 30;
             transportistaLabel.Text = "Transportista";
             // 
@@ -288,10 +277,9 @@
             // 
             transportistaBox.DropDownStyle = ComboBoxStyle.DropDownList;
             transportistaBox.FormattingEnabled = true;
-            transportistaBox.Location = new Point(375, 215);
-            transportistaBox.Margin = new Padding(3, 4, 3, 4);
+            transportistaBox.Location = new Point(328, 161);
             transportistaBox.Name = "transportistaBox";
-            transportistaBox.Size = new Size(138, 28);
+            transportistaBox.Size = new Size(121, 23);
             transportistaBox.TabIndex = 31;
             // 
             // panel1
@@ -299,26 +287,28 @@
             panel1.BackColor = Color.FromArgb(161, 32, 89);
             panel1.Controls.Add(pictureBox1);
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(765, 37);
+            panel1.Size = new Size(669, 28);
             panel1.TabIndex = 32;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.GrupoE;
-            pictureBox1.Location = new Point(-34, 9);
+            pictureBox1.Location = new Point(-30, 7);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(174, 25);
+            pictureBox1.Size = new Size(152, 19);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // FormOrdenDePreparacion
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonHighlight;
-            ClientSize = new Size(763, 685);
+            ClientSize = new Size(668, 514);
             Controls.Add(panel1);
             Controls.Add(transportistaBox);
             Controls.Add(transportistaLabel);
@@ -341,7 +331,6 @@
             Controls.Add(DetalleOrden);
             Controls.Add(DetallesList);
             Controls.Add(Orden);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "FormOrdenDePreparacion";
             Text = "Ordenes";
             Load += Ordenes_Load;
